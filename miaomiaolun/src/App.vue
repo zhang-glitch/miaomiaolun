d20b22c855cf3bc38bf9bfc6383aa2dc4bba5dab
zhang-glitch <669638556@qq.com>
2020-11-11 21:49:43 +0800

    column-list分支开发完毕

--- miaomiaolun/src/App.vue
+++ miaomiaolun/src/App.vue
@@ -1,11 +1,55 @@
 <template>
+  <div class="app">
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import ColumnList ,{ColumnProps}from './components/ColumnList.vue'
+const list: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'iiiiiiiiiiiii',
+    avatar: '',
+    description: 'dwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+  },
+  {
+    id: 2,
+    title: 'iiiiiiiiiiiii',
+    avatar: 'https://pic2.zhimg.com/80/v2-6cc71aa528262ee99461fc57eaafe869_720w.jpg?source=1940ef5c',
+    description: 'dwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+  },
+  {
+    id: 3,
+    title: 'iiiiiiiiiiiii',
+    avatar: 'https://pic2.zhimg.com/80/v2-6cc71aa528262ee99461fc57eaafe869_720w.jpg?source=1940ef5c',
+    description: 'dwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+  },
+  {
+    id: 4,
+    title: 'iiiiiiiiiiiii',
+    avatar: 'https://pic2.zhimg.com/80/v2-6cc71aa528262ee99461fc57eaafe869_720w.jpg?source=1940ef5c',
+    description: 'dwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+  },
+  {
+    id: 5,
+    title: 'iiiiiiiiiiiii',
+    avatar: 'https://pic2.zhimg.com/80/v2-6cc71aa528262ee99461fc57eaafe869_720w.jpg?source=1940ef5c',
+    description: 'dwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+  }
+] 
 export default defineComponent({
   name: 'App',
+  components: {
+    ColumnList
+  },
+  setup() {
+    return {
+      list
+    }
+  }
 })
 </script>
 
---
+++ miaomiaolun/src/components/ColumnList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="colum-list">
+    <div class="container">
+      <div class="row ">
+        <div class="col-4 mb-4"  v-for="item in columnList" :key="item.id">
+          <div class="card h-100 shadow-sm">
+            <div class="card-body text-center">
+              <img class="rounded-circle border border-light w-25 my-3" :src="item.avatar" alt="">
+              <h5 class="card-title">{{item.title}}</h5>
+              <p class="card-text text-left">{{item.description}}</p>
+              <a class="btn btn-primary">进入专栏</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, PropType, computed} from 'vue';
+export interface ColumnProps {
+  id: number,
+  title: string,
+  avatar: string,
+  description: string,
+};
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  },
+  setup(props) {
+    const columnList = computed(() => {
+      return props.list.map(item => {
+        if(!item.avatar) {
+          item.avatar = require("../assets/avatar.jpg")
+        }
+        return item
+      })
+    })
+
+    return {
+      columnList
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
